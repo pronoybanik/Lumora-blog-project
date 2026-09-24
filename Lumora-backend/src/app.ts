69e6ad741d928ad1097927dde7bd187a6a779da5
Pronoy Banik <pronoybanik82@gmail.com>
2026-09-24 23:41:10 +0600

    change deply code

--- Lumora-backend/src/app.ts
+++ Lumora-backend/src/app.ts
@@ -4,14 +4,14 @@ import express, {
   type Response,
 } from "express";
 import router from "./app/router";
-import cors from "cors"
+import cors from "cors";
 
 const app = express();
 
 app.disable("x-powered-by");
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
